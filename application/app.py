d45d484fd6cd813fa836676bc0eab27ec54ba272
Kasey <kmk220000@utdallas.edu>
2024-11-27 02:31:52 -0600

    opens window with base img when theme is specified

--- application/app.py
+++ application/app.py
@@ -3,6 +3,9 @@ import datetime
 import PIL
 import os
 
+import PIL.Image
+import PIL.ImageTk
+
 # def welcome_text():
     # tk.label.config()
 
@@ -12,31 +15,39 @@ def welcome_screen():
     welcome = tk.Tk()
     welcome.geometry("300x200")
     welcome.title("Welcome!")
-    welcome_message = tk.Label(welcome, text="Choose a Theme!",
+    welcome_message = tk.Label(welcome, text="VTuber Schedule Builder",
                                       font=("Arial", 18))
     welcome_message.pack()
 
     themes = os.listdir("themes")
-    clicked = tk.StringVar()
-    clicked.set(" ")
-    choose = tk.OptionMenu(welcome, clicked, themes)
+    clicked = tk.StringVar(welcome)
+    clicked.set("Choose a Theme")
+    choose = tk.OptionMenu(welcome, clicked, *themes)
     choose.pack()
-    choose.config(text=clicked.get())
+    # choose.config(text=clicked.get())
 
     def launch():
+        theme = clicked.get()
         welcome.destroy()
-        schedule_screen()
+        schedule_screen(theme)
+        return theme
 
     start = tk.Button(welcome, text="Start", command=launch)
     start.pack()
     
     welcome.mainloop()
 
-def schedule_screen():
+def schedule_screen(theme):
     schedule = tk.Tk()
     schedule.geometry("1920x1080")
-    schedule.title("Schedule Builder")
+    schedule.title("VTuber Schedule Builder")
     schedule.configure(bg="black")
+    # print(theme)
+    with PIL.Image.open(f'themes/{theme}/{theme} base.png') as base_open:
+        base_img = PIL.ImageTk.PhotoImage(base_open)
+    base = tk.Label(schedule, image=base_img)
+    base.pack()
+
     schedule.mainloop()
 
 def main():
